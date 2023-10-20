265ea8869d5707f282148996fccfae66b88e080f
Bilqis002 <ahmedb1009@bowiestate.edu>
2023-10-19 22:46:49 -0400

    Lab3

---
+++ Lab 3.py
@@ -0,0 +1,52 @@
+import math
+
+number1=input('Enter number1')
+number2=input('Enter number2')
+number3=input('Enter number3')
+print(number1, number2)
+sum=number1+number2
+sum=int(number1)+int(number2)
+print(sum)
+#difference=number2-number1
+difference=int(number2)-int(number1)
+print(difference)
+multiply=int(number1)*int(number2)
+print(multiply)
+division=int(number2)/int(number1)
+print(division)
+#print(sum,difference,multiply,division)
+print(sum,difference,multiply,division)
+
+sum=int(number1)+int(number2)+int(number3)
+print(sum)
+difference=int(number1)-int(number2)-int(number3)
+print(difference)
+multiply=int(number1)*int(number2)*int(number3)
+print(multiply)
+Division=int(number1)/int(number2)/int(number3)
+print(division)
+#Return the value of 9 raised to the power 3
+print(math.pow(9,3))
+import math
+x=math.sqrt(81)
+print(x)
+x=math.ceil(8.2)
+print(x)
+y=math.floor(3.9)
+print(y)
+x=math.log10(3.5)
+print(x)
+y=math.pi
+print(y)
+y=math.copysign(4,8)
+print(y)
+x=math.fabs(8.4)
+print(x)
+x=math.sin(4.8)
+print(x)
+x=math.log2(10)
+print(x)
+y=math.factorial(25)
+print(y)
+
+
